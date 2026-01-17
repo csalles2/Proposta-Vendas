8826d50ca973b696d7366f6cd4f037988aa6d7d4
Camille Sales <camilleaguirre743@gmail.com>
2026-01-17 17:15:25 -0400

    tirando botoes

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function contatar() {
-    // O formato correto deve ser: CodigoPais (55) + DDD (69) + Numero
-    const fone = "5569992308825"; 
-    const mensagem = "Olá! Recebi a proposta do site Magnus e gostaria de fechar o projeto.";
-    
-    // Usamos o link oficial api.whatsapp.com que é mais estável para navegadores
-    const url = "https://api.whatsapp.com/send?phone=" + fone + "&text=" + encodeURIComponent(mensagem);
-    
-    window.open(url, '_blank');
-}
+
 
 // Efeito de scroll suave para links internos
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
