c3355eb2358ccb6d0c0a29a9af7745f68e7bdde4
csalles2 <camilleaguirre743@gmail.com>
2026-01-21 14:14:52 -0400

    Alterações versão da proposta comercial

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// Efeito de scroll suave para links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
